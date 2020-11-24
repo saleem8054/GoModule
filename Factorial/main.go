8bc75f87434b38e992352608616a9f6eeef467df
saleem8054 <transparent79twister@gmail.com>
2020-11-24 19:00:27 +0100

    major version 3

--- Factorial/main.go
+++ Factorial/main.go
@@ -1,8 +1,8 @@
 package Factorial
-func Factorial(n int) int {
+func Factorial(n int) (int,string) {
     var f int = 1
     for i := 2; i <= n; i++ {
         f *= i
     }
-    return f
+    return f,"v3 executed"
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/saleem8054/GoModule/v2
+module github.com/saleem8054/GoModule/v3
 
 go 1.14
